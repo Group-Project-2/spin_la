112986d209d71c115c23c0097c0a09b3cc1eead9
creaboulait <norshida.razali@gmail.com>
2018-07-24 17:56:26 +0800

    Config Clearance controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+  #Change path when needed
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'deals#index', as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: "clearance/sessions#new"
+  end
+
+resources :deals
+
 end
