3b320d88da67d280ff94ea8fdba95f914a83f174
creaboulait <norshida.razali@gmail.com>
2018-07-25 23:40:16 +0800

    Fix sign out path to welcome index page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   # end
 
   constraints Clearance::Constraints::SignedOut.new do
-    root to: "clearance/sessions#new"
+    root to: "welcome#index"
   end
 
 resources :deals
