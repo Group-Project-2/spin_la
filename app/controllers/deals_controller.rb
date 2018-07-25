2a7eaebceae22c324c2ca37ee5bf9e62da6c82d2
creaboulait <norshida.razali@gmail.com>
2018-07-25 17:37:45 +0800

    Add JS to deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,10 +1,8 @@
 class DealsController < ApplicationController
 
 	def index
-		if current_user.role == "business"
-			@company = Company.find_by(user_id: current_user.id)
-			redirect_to company_path(@company)
-		end
+		@user = current_user
+		@deals = Deal.all
 	end
 
 	def new
@@ -20,6 +18,25 @@ class DealsController < ApplicationController
 			redirect_to company_path(@company)
 		else
 			redirect_back
+		end
+	end
+
+	def show
+		@deal = Deal.find(params[:id])
+	end
+
+	def spin
+		if current_user.spins_remaining != 0
+			current_user.spins_remaining -= 1
+			current_user.update(spins_remaining: current_user.spins_remaining)
+			respond_to do |format|
+				format.js
+			end
+		elsif current_user.spins_remaining == 0
+		   flash[:alert] = "Boo! No more spins available!"
+		   respond_to do |format|
+				format.js
+			end
 		end
 	end
 
