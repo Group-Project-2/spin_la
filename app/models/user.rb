7984819e761389a4bbc364e5e26ece5cd0433d4c
creaboulait <norshida.razali@gmail.com>
2018-07-25 10:53:59 +0800

    Add welcome controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 	include Clearance::User
  	has_many :authentications, dependent: :destroy
 
+
 	def self.create_with_auth_and_hash(authentication, auth_hash)
 		user = self.create!(
 		 first_name: auth_hash["info"]["given_name"],
