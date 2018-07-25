1fbcd1656713af1247043bd42fda892f53c515c7
creaboulait <norshida.razali@gmail.com>
2018-07-25 20:40:39 +0800

    Add basic random logic

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -26,9 +26,22 @@ class DealsController < ApplicationController
 	end
 
 	def spin
+		@deal = Deal.find(params[:id])
+		@win_odds = ((@deal.odds_numerator.to_f)/(@deal.odds_denominator.to_f))*100
+
 		if current_user.spins_remaining != 0
-			current_user.spins_remaining -= 1
-			current_user.update(spins_remaining: current_user.spins_remaining)
+			current_user.update(spins_remaining: current_user.spins_remaining -= 1)
+		
+			# logiclogiclogic
+			case rand (100)+1
+				when 1..@win_odds
+					@deal.update(wins_remaining: @deal.wins_remaining -= 1)
+					current_user.update(deals_won: current_user.deals_won << @deal.id)
+					#Alert user they won
+				when @win_odds..100
+					#Alert user they lost
+			end
+	
 			respond_to do |format|
 				format.js
 			end
@@ -41,6 +54,7 @@ class DealsController < ApplicationController
 	end
 
 	private
+
 
 	def deal_params
 		params.require(:deal).permit(:wins_remaining, :odds_numerator, :odds_denominator, :description)
