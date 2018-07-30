d8ea47a7e44295795ebb85004a4e3ce738f18630
creaboulait <norshida.razali@gmail.com>
2018-07-30 14:22:17 +0800

    Add edit method to User profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < Clearance::UsersController
+	before_action :set_user, only: [:show, :edit, :update]
 
 	def user_from_params
 		email = user_params.delete(:email)
@@ -16,11 +17,26 @@ class UsersController < Clearance::UsersController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		@user.update(user_params)
+		redirect_to user_path(@user)
 	end
 
 	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+		
+	end
 	def user_params
-		params.require(:user).permit(:email, :password, :first_name, :last_name)
+		params.require(:user).permit(:email, :password, :first_name, :last_name, :profilepic)
 	end
 end
