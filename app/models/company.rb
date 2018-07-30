7d055df35838bdf3c2ae26f7857d23fbfb23ecb5
creaboulait <norshida.razali@gmail.com>
2018-07-30 18:36:55 +0800

    Add images uploaded to Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,5 @@ class Company < ApplicationRecord
 	has_many :deals, dependent: :destroy
 	has_many :reviews, dependent: :destroy
 	belongs_to :user
-	# mount_uploaders :avatars, AvatarUploader
+	mount_uploaders :avatars, AvatarUploader
 end
